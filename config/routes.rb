7a0449a96719bf70ac5ae3960f3cc37c215340c3
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-24 00:06:19 +0900

    ルーティング修正 roots -> root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users, only: %i[index show edit update]
-  roots "users#index"
+  root "users#index"
 end
