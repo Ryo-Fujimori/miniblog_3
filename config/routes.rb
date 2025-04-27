8e04ec64cdb068debfad0d7adb90b33df7fe25e4
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-27 18:08:34 +0900

    Postコントローラー作成、ポスト一覧画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users, only: %i[index show edit update]
+  resources :posts
   root "users#index"
 end
