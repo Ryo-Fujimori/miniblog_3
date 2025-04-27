5cc4ecfded9aca3ef572073b25678b2bc50748f8
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-27 18:42:57 +0900

    140字制限が2文字になっていたので修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :content, presence: :true, length: {maximum: 2}
+  validates :content, presence: :true, length: {maximum: 140}
   validates :user_id, presence: true
 end
