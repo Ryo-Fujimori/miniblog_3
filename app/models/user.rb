562da25e37605488ac76ae2579538547e9d93c7a
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-29 21:46:23 +0900

    レビュー修正　deviseは一度しか書かないのに比べ、has_manyは複数回書くことがあるので4行目に移動

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy
   validates :profile, length: { maximum: 200 }
 end
