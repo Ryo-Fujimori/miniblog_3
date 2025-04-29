119d997c20d52048327583ee20a98f0d17a1bba6
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-29 18:38:07 +0900

    誤字修正 maximum: 2 → maximum: 200

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :content, presence: :true, length: {maximum: 2}
+  validates :content, presence: :true, length: {maximum: 200}
   validates :user_id, presence: true
 end
