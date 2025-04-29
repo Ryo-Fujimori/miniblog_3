04f3bfffdcb43d320b6f75028fdf424ca2252730
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-29 22:19:33 +0900

    beforeactionの構文が間違っていたので修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[:show :edit :update :destroy]
-  before_action :authenticate_user!, except: %i[:index :show]
-  before_action :correct_user, only: %i[:edit :update :destroy]
-
+  before_action :set_post, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :correct_user, only: %i[edit update destroy]
+  
   def index
     @posts = Post.all
   end
