18b7033d1bab6c585a98c8dc0c423819b38d0662
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-29 21:22:57 +0900

    レビュー修正　before_action内のonlyを%iを使用して書くように統一

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :set_post, only: %i[:show :edit :update :destroy]
+  before_action :authenticate_user!, except: %i[:index :show]
+  before_action :correct_user, only: %i[:edit :update :destroy]
 
   def index
     @posts = Post.all
