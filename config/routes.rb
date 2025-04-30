0b220932a14591eb6cf278c70684b55deb8851f6
Ryo-Fujimori <fujimori1072@gmail.com>
2025-05-01 02:15:14 +0900

    フォロー、フォロー解除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :users, only: %i[index show edit update]
   
-  # ログインユーザー向けのusers/postsコントローラー
+  # ログインユーザー向けのusers/posts, relationshipsコントローラー
   namespace :users do
     resources :posts, only: %i[new create edit update destroy]
+    resources :relationships, only: %i[create destroy]
   end
-
+  
   # 一般ユーザー向けのpostsコントローラー
   resources :posts, only: %i[index show]
   
+  resources :users, only: %i[index show edit update]
   root "posts#index"
 end
