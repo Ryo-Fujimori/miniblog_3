a35f4261be63c3bd6d01c6350a731341d8fc7f74
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-30 18:44:01 +0900

    Relationshipモデル作成

---
+++ app/models/Relationship.rb
@@ -0,0 +1,18 @@
+class Relationship < ApplicationRecord
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followed, class_name: 'User'
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+
+  # 2回フォローできないようにする
+  validates :follower_id, uniqueness: { scope: :followed_id }
+  # 自分自身をフォローできないようにする
+  validate :cannot_follow_self
+
+
+  private
+
+  def cannot_follow_self
+    errors.add(:base, "You cannot follow yourself") if follower_id == followed_id
+  end
+end
