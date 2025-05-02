b73955b270660bb3268ccb23f0db06223ba7f0b5
Ryo-Fujimori <fujimori1072@gmail.com>
2025-05-02 16:35:57 +0900

    Postのテスト記述

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
+# require 'rspec/rails'
+# support 配下のfileを読み込む
+Dir[Rails.root.join('spec/support/*.rb')].each { |f| require f }
 
-RSpec.describe "Posts", type: :system do
-  before do
-    driven_by(:rack_test)
+describe 'Posts', type: :system do
+  
+  it '新しい投稿を作成できる' do
+    # 投稿作成ページに移動
+    visit new_post_path
+
+    # フォームに入力
+    fill_in 'Content', with: '投稿の内容'
+
+    # 作成ボタンをクリック
+    click_button 'submit'
+
+    # 結果を確認
+    expect(page).to have_content('投稿が作成されました')
+    expect(page).to have_content('新しい投稿')
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
